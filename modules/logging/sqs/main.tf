dde7e3517c04cd70ea94b12084e0356884a72abd
kali prasad <kaliprasadeducation@gmail.com>
2023-08-03 18:12:53 +0000

    update main

--- modules/logging/sqs/main.tf
+++ modules/logging/sqs/main.tf
@@ -1,5 +1,3 @@
-# modules/sqs/main.tf
-
 resource "aws_sqs_queue" "app_queue" {
   for_each                  = var.apps
 
@@ -9,6 +7,12 @@ resource "aws_sqs_queue" "app_queue" {
   message_retention_seconds = each.value["message_retention_seconds"]
   receive_wait_time_seconds = each.value["receive_wait_time_seconds"]
   visibility_timeout_seconds= each.value["visibility_timeout_seconds"]
+  fifo_queue                = each.value["fifo_queue"]
+  content_based_deduplication = each.value["content_based_deduplication"]
+  sqs_managed_sse_enabled   = each.value["sqs_managed_sse_enabled"]
+  deduplication_scope       = each.value["deduplication_scope"]
+  fifo_throughput_limit     = each.value["fifo_throughput_limit"]
+  redrive_policy            = each.value["redrive_policy"]
 
   tags = merge(
     var.tags
